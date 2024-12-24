3c02674e44860348205d1f820241b0ca71013e5a
Jhonny Macedo <jhonjhonny.rr@gmail.com>
2024-12-24 10:56:58 -0400

    Class created ContaReceber, ContaPagar and StatusContaPagar

---
+++ src/main/java/com/loja/virtual/enums/StatusContaPagar.java
@@ -0,0 +1,28 @@
+package com.loja.virtual.enums;
+
+public enum StatusContaPagar {
+	
+	COBRANCA("Pagar"),
+	VENCIDA("Vencida"),
+	ABERTA("Aberta"),
+	QUITADA("Quitada"),
+	NEGOCIADA("ReNegociada");
+	
+	private String descricao;
+	
+	private StatusContaPagar(String descricao) {
+		this.descricao = descricao;
+		
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return this.descricao;
+	}
+
+}
