c3f24c6f0dbae22acdb71df17b8cc9b5abd1e3cb
Jhonny Macedo <jhonjhonny.rr@gmail.com>
2024-12-26 14:16:49 -0400

    class created: Produto, ImagemProduto, NotaFiscalProduto, NotaItemProduto

--- src/main/java/com/loja/virtual/LojaVirtualApplication.java
+++ src/main/java/com/loja/virtual/LojaVirtualApplication.java
@@ -2,8 +2,10 @@ package com.loja.virtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*import org.springframework.boot.autoconfigure.domain.EntityScan;*/
 
 @SpringBootApplication
+/*@EntityScan(basePackages = "loja-virtual")*/
 public class LojaVirtualApplication {
 
 	public static void main(String[] args) {
