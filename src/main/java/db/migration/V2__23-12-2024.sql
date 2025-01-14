9e274c2a0f574b5516ab6a5ab88eb75c37ab9f4b
Jhonny Macedo <jhonjhonny.rr@gmail.com>
2025-01-13 20:12:22 -0400

    created test, development, production profiles and flyway configuration

--- src/main/java/db/migration/V2__23-12-2024.sql
+++ src/main/java/db/migration/V2__23-12-2024.sql
@@ -1,23 +1,11 @@
-select constraint_name from information_schema.constraint_column_usage
- where table_name = 'usuarios_acesso' and column_name = 'acesso_id'
- and constraint_name <> 'unique_acesso_user';
- 
- alter table usuarios_acesso drop CONSTRAINT "uk8bak9jswon2id2jbunuqlfl9e";
- 
- select constraint_name
-FROM information_schema.table_constraints
-WHERE table_name = 'nota_fiscal_venda';
+-- Remover restrições da tabela `usuarios_acesso`, exceto a `unique_acesso_user`
+ALTER TABLE public.usuarios_acesso
+DROP CONSTRAINT IF EXISTS uk8bak9jswon2id2jbunuqlfl9e;
 
-ALTER TABLE public.nota_fiscal_venda 
-DROP CONSTRAINT uklwo38kb0aaxboalphfjlan8qr;
+-- Remover restrição da tabela `nota_fiscal_venda`
+ALTER TABLE public.nota_fiscal_venda
+DROP CONSTRAINT IF EXISTS uklwo38kb0aaxboalphfjlan8qr;
 
-SELECT constraint_name
-FROM information_schema.table_constraints
-WHERE table_name = 'vd_cp_loja_virt';
-
-ALTER TABLE public.vd_cp_loja_virt 
-DROP CONSTRAINT ukhkxjejv08kldx994j4serhrbu;
-
-
+-- Remover restrição da tabela `vd_cp_loja_virt`
 ALTER TABLE public.vd_cp_loja_virt
-DROP CONSTRAINT ukhkxjejv08kldx994j4serhrbu;
+DROP CONSTRAINT IF EXISTS ukhkxjejv08kldx994j4serhrbu;
